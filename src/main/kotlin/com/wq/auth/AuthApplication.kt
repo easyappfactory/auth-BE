6f9176f751317f4f4b7b7180ea636b904b46dbda
Mint <jrkd2165@gmail.com>
2025-11-04 15:13:41 +0900

    feat: RateLimiter 구현 (토큰 버킷)

--- src/main/kotlin/com/wq/auth/AuthApplication.kt
+++ src/main/kotlin/com/wq/auth/AuthApplication.kt
@@ -3,7 +3,9 @@ package com.wq.auth
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class AuthApplication
